038467c70f4861b75e10c54b814f8af4f109b631
Derek DeRaps <hawkeye@tenderwolf.io>
2017-10-09 20:09:09 -0400

    Improve module install steps and move code
    Moving the code will facilitate writing tests later.

--- src/KalaUtil.php
+++ src/KalaUtil.php
@@ -1,11 +1,11 @@
 <?php
 
-/**
- * @file
- * Contains class KalaUtil.
- */
+namespace Drupal\kalaconfig;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\config_filter\Config\FilteredStorage;
+use Drupal\config_split\Config\GhostStorage;
+use Drupal\Core\Config\FileStorageFactory;
 
 /**
  * Various helpful functions for use mainly by Drupal settings files.
@@ -101,6 +101,59 @@ class KalaUtil {
     foreach ($wildcard_to_path as $wildcard => $path) {
       if (stripos($_SERVER['REQUEST_URI'], $wildcard) === 0) {
         static::redirect(NULL, NULL, $path);
+      }
+    }
+  }
+
+  /**
+   * Exports a given config split to its corresponding folder.
+   *
+   * Code adapted from \Drupal\config_split\ConfigSplitCliService::ioExport().
+   *
+   * @param array $splits
+   *   Machine names of the config splits to export. E.g., ['dev', 'test'].
+   */
+  public static function exportConfigSplits(array $splits) {
+    $config_filter = \Drupal::service('plugin.manager.config_filter');
+    $storage_factory = \Drupal::service('config_filter.storage_factory');
+    $sync = FileStorageFactory::getSync();
+    $config_split = \Drupal::service('config_split.cli');
+    foreach ($splits as $split) {
+      $plugin_id = "config_split:$split";
+      $filter = $config_filter->getFilterInstance($plugin_id);
+      $storage = $storage_factory->getFilteredStorage($sync, ['config.storage.sync'], [$plugin_id]);
+      $split = new FilteredStorage(new GhostStorage($storage), [$filter]);
+      $config_split->export($split);
+    }
+  }
+
+  /**
+   * Deletes a content type.
+   *
+   * @param string $name
+   *   Machine name of the content type to delete.
+   */
+  public static function deleteContentType($name) {
+    if ($type = \Drupal::entityManager()->getStorage('node_type')->load($name)) {
+      $type->delete();
+    }
+
+    // Run cron to purge field tables from the database.
+    \Drupal::service('cron')->run();
+  }
+
+  /**
+   * Deletes all the entities of the given entity type.
+   *
+   * @param string $entity_type
+   *   The entity type machine name.
+   */
+  public static function deleteAllEntitiesOfType($entity_type) {
+    if (\Drupal::entityTypeManager()->hasHandler($entity_type, 'storage')) {
+      $storage = \Drupal::entityTypeManager()->getStorage($entity_type);
+      if ($ids = $storage->getQuery()->execute()) {
+        $entities = $storage->loadMultiple($ids);
+        $storage->delete($entities);
       }
     }
   }
